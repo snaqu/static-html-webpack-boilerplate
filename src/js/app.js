9fb99e180a2c6e8d7e28812142eb1d400627f8b6
pawelmiklas <pablomiklas@gmail.com>
2018-12-14 02:15:56 +0100

    add partials, sass path fixed, install aos

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import 'bootstrap';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 // if (module.hot) {
 // 	module.hot.accept();
 // }
--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,12 +4,15 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+// const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const generateHTMLPlugins = () =>
 	glob.sync('./src/**/*.html').map(dir =>
 		new HTMLWebpackPlugin({
 			filename: path.basename(dir), // Output
 			template: dir, // Input
+			minify: {
+				collapseWhitespace: true
+			}
 		}));
 
 module.exports = {
@@ -28,13 +31,36 @@ module.exports = {
 				loader: 'babel-loader',
 			},
 			{
-				test: /\.(sass|scss)$/,
-				use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+				test: /\.(sass|scss|css)$/,
+				use: ['style-loader', 'css-loader?url=false', 'postcss-loader', 'sass-loader'],
 			},
 			{
 				test: /\.html$/,
 				loader: 'raw-loader',
 			},
+			// {
+			// 	test: /\.(gif|png|jpe?g|svg)$/i,
+			// 	use: [
+			// 		{
+			// 			loader: 'file-loader',
+			// 			options: {
+			// 				name: '[name].[ext]',
+			// 				outputPath: 'static/'
+			// 			}
+			// 		},
+			// 		{
+			// 			loader: 'image-webpack-loader',
+			// 			options: {
+			// 				mozjpeg: {
+			// 					quality: 65
+			// 				},
+			// 				pngquant: {
+			// 					quality: '65-90'
+			// 				}
+			// 			}
+			// 		},
+			// 	]
+			// },
 		],
 	},
 	plugins: [
